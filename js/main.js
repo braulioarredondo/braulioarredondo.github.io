c8f9c5f08bf9ff989c626f7c946d5732c5795b74
Braulio Arredondo <31326976+braulioarredondo@users.noreply.github.com>
2020-12-28 13:22:57 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$('#submitBtn').click(function() {
+    /* when the button in the form, display the entered values in the modal */
+    $(".alert").hide().show('medium');
+
+    $('#formContact').submit();
+});
+
+function changeResume(parameter){
+    if(parameter){
+        var new_url = "files/cvEsp.pdf";
+            $('#resumeShow').attr('data', new_url);
+            $('#resumeAlt').attr('href', new_url);
+            //$('#resumeShow').load(new_url);
+            //$('#resumeAlt').load(new_url);
+    }else{
+        var new_url = "files/cvEng.pdf";
+            $('#resumeShow').attr('data', new_url);
+            $('#resumeAlt').attr('href', new_url);
+    }
+}
